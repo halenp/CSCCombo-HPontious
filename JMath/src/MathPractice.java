6c5d2c1312336f809b278d745f538c4c728cf3d1
Pontious <S02909444@ccc.ccofc.edu>
2024-01-30 11:46:46 -0700

    JMath

---
+++ JMath/src/MathPractice.java
@@ -0,0 +1,18 @@
+
+public class MathPractice {
+
+	public static void main(String[] args) {
+		// Simple addition demo
+		int num1 = 23;
+		
+		int num2 = 20;
+		
+		int ans;
+		
+		ans = num1 + num2;
+		
+		System.out.println("The answer is " + ans);
+		
+	}
+
+}
